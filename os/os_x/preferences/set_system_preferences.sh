47090753f148ba79f150855b52369ea859068301
Ankrat <y.lecorre.mail@gmail.com>
2018-01-18 21:58:44 +0100

    Not permitted actions
    Commented out not permitted actions
    Added sudo to `pmset` for hdd hibernation parameters

--- os/os_x/preferences/set_system_preferences.sh
+++ os/os_x/preferences/set_system_preferences.sh
@@ -10,24 +10,32 @@ set_preferences() {
     execute "defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true" \
         "Prevent Time Machine from prompting to use new hard drives as backup volume"
 
+    execute "defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool no" \
+        "Disable power button from putting your mac in Stand-by mode"
+
     ###############################################################################
     # SSD-specific tweaks                                                         #
     ###############################################################################
 
-    execute "pmset -a hibernatemode 0" \
+    execute "sudo pmset -a hibernatemode 0" \
         "Disable hibernation (speeds up entering sleep mode)"
 
-    execute "rm /private/var/vm/sleepimage" \
-        "Remove the sleep image file to save disk space"
 
-    execute "touch /private/var/vm/sleepimage" \
-        "Create a zero-byte file instead..."
-
-    execute "chflags uchg /private/var/vm/sleepimage" \
-        "...and make sure it can’t be rewritten"
-
-    execute "pmset -a sms 0" \
+    execute "sudo pmset -a sms 0" \
         "Disable the sudden motion sensor as it’s not useful for SSDs"
+
+    ###############################################################################
+    # NOT PERMITTED
+    ###############################################################################
+    # execute "rm /private/var/vm/sleepimage" \
+    #     "Remove the sleep image file to save disk space"
+
+    # execute "touch /private/var/vm/sleepimage" \
+    #     "Create a zero-byte file instead..."
+
+    # execute "chflags uchg /private/var/vm/sleepimage" \
+    #     "...and make sure it can’t be rewritten"
+
 
 }
 
